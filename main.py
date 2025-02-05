cae228804bd72102f6d9e3ca9ea72dd3b319916f
ea2097 <ea2097@bath.ac.uk>
2025-02-05 09:41:45 +0200

    enable error handler

--- main.py
+++ main.py
@@ -14,25 +14,25 @@ while True:
     print(" 7. View/Update Destination Information")
     print(" *. Exit\n")
 
-    # try:
-    __choose_menu = input("Enter your choice: ")
-    if __choose_menu == "1":
-        db_ops.select_views()
-    elif __choose_menu == "2":
-        db_ops.select_tables()
-    elif __choose_menu == "3":
-        db_ops.select_all()
-    elif __choose_menu == "4":
-        db_ops.search_data()
-    elif __choose_menu == "5":
-        db_ops.update_data()
-    elif __choose_menu == "6":
-        db_ops.delete_data()
-    elif __choose_menu == "*":
-        exit(0)
-    else:
+    try:
+        __choose_menu = input("Enter your choice: ")
+        if __choose_menu == "1":
+            db_ops.select_views()
+        elif __choose_menu == "2":
+            db_ops.select_tables()
+        elif __choose_menu == "3":
+            db_ops.select_all()
+        elif __choose_menu == "4":
+            db_ops.search_data()
+        elif __choose_menu == "5":
+            db_ops.update_data()
+        elif __choose_menu == "6":
+            db_ops.delete_data()
+        elif __choose_menu == "*":
+            exit(0)
+        else:
+            print("Invalid Choice")
+    except Exception as e:
+        print(e)
         print("Invalid Choice")
-    # except Exception as e:
-    #     print(e)
-    #     print("Invalid Choice")
-    #     continue
+        continue
